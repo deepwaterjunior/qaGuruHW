51d19215ce0b74972f6813f5dbaa206a06d8a129
Dmitrii <forpaymen7romanroro@yandex.ru>
2023-03-29 23:53:34 +0300

    Selenide test. Student Registration Form complation: comments added, hobbies

--- src/test/java/CheckStudentRegistrationForm.java
+++ src/test/java/CheckStudentRegistrationForm.java
@@ -1,50 +1,54 @@
 package testing;
 
+import FormTestConfig.FormTestConfig;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import org.openqa.selenium.By;
 
 
-public class CheckStudentRegistrationForm {
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
+
+public class CheckStudentRegistrationForm extends FormTestConfig {
 
     @Test
-    void checkRegistrationForm(){
-        Configuration.holdBrowserOpen = true;
+     void checkRegistrationForm(){
+
 
         // Открыли страницу
         open("https://demoqa.com/automation-practice-form");
-
         // По id заполнили имя
-        $("[id=firstName]").setValue("Ivan123");
-
+        $("#firstName").setValue("Ivan123");
         //  По id заполнили фамилию
-        $("[id=lastName]").setValue("Ivan0V321~");
-
+        $("#lastName").setValue("Ivan0V321~");
         //  По id заполнили Email
-        $("[id=userEmail]").setValue("N_a-t.m+e@e_X-a.m0ple.coM52");
-
+        $("#userEmail").setValue("N_a-t.m+e@e_X-a.m0ple.coM52");
         //  Кликнули по радиобатону gender
-        $("[class=custom-control-label]").click();
-
+        $(".custom-control-label").click();
         //  ввели номер телефона студента
-        $("[id=userNumber]").setValue("0123456789");
-
+        $("#userNumber").setValue("0123456789");
         // нашли инпут календаря и кликнули по нему для раскртия таблицы
-        $("[class=react-datepicker-wrapper]").click();
-
+        $(".react-datepicker-wrapper").click();
         // Нашли дроп-даун (селект) и выбрали по тексту нужный месяц
-        $("[class=react-datepicker__month-select]").selectOptionContainingText("April");
-
+        $(".react-datepicker__month-select").selectOptionContainingText("April");
         // Нашли дроп-даун (селект) и выбрали по тексту нужный год
-        $("[class=react-datepicker__year-select]").selectOptionByValue("1994");
-
+        $(".react-datepicker__year-select").selectOptionByValue("1994");
         /* Нашли нужный день в таблице по классу и кликнули по нему (хотел как-то иначе, по контексту aria-label или значению
         но не понял как реализовать*/
         $(".react-datepicker__day--024").click();
+        //  ввели название предмета студента
+        $("#subjectsInput").setValue("History").pressEnter();
+        //  выбрали хобби
+        $(byText("Music")).click();
+        $(byText("Reading")).click();
+
+        $("#uploadPicture").uploadFile();
+
     }
 
 }
